05da8a6850f337424bee34015fede34d6bce8770
Kurt.Wall <kurt.wall@surescripts.com>
2023-11-08 15:26:37 -0800

    Initial check-in

---
+++ forum/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.CASCADE,
+                             related_name="posts")
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.CASCADE,
+                             related_name="comments")
+    post = models.ForeignKey(Post, blank=True, null=True, on_delete=models.CASCADE, related_name="comments")
+    body = models.TextField(blank=True, null=True)
