9ae6f690290064ed3d007c73d3bf2ca789b28a88
Kurt.Wall <kurt.wall@surescripts.com>
2023-11-09 14:25:08 -0800

    Add class-based view for comments

--- forum/urls.py
+++ forum/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-from .views import index, PostDetailView, PostListView
+from .views import index, CommentListView, PostDetailView, PostListView
 
 urlpatterns = [
     # ex: /forum/
@@ -7,16 +7,16 @@ urlpatterns = [
 
     # ex: /forum/5/
     path('<int:pk>/', PostDetailView.as_view(), name='detail'),
-    #
+
     # # ex: /forum/5/comment/
-    # path('<int:post_id>/comment/', 'views.comment', name='comment'),
-    #
+    path('<int:post_id>/comment/', CommentListView.as_view(), name='comment'),
+
     # ex: /posts/2023/
     re_path(r'^posts/(?P<year>[0-9]{4})/$', PostListView.as_view(), name='by_year'),
 
     # ex /forum/posts/2023/03/
     re_path(r'^posts/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', PostListView.as_view(), name='by_month'),
-    #
+
     # # ex /forum/posts/2023/03/post-slug/
     # re_path(r'^posts/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<slug>[\w+])$', 'views.post_detail', name='slug_detail'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -6,26 +6,27 @@ from forum.models import Comment, Post
 
 
 def index(request):
-    return HttpResponse("<html><body>Our first response</body></html>")
+    return HttpResponse('<html><body>Our first response</body></html>')
 
 
 class PostListView(ListView):
     model = Post
-    context_object_name = "post_data"
+    context_object_name = 'post_data'
 
     def get_queryset(self):
         self.queryset = Post.objects.all()
-        if self.kwargs.get("year"):
-            self.queryset = self.queryset.filter(created_at__year=self.kwargs["year"])
-        if self.kwargs.get("month"):
-            self.queryset = self.queryset.filter(created_at__month=self.kwargs["month"])
+        if self.kwargs.get('year'):
+            self.queryset = self.queryset.filter(created_at__year=self.kwargs['year'])
+        if self.kwargs.get('month'):
+            self.queryset = self.queryset.filter(created_at__month=self.kwargs['month'])
         return self.queryset
 
     def render_to_response(self, context, **response_kwargs):
-        posts = ""
-        for post in context["post_data"]:
-            posts += f"<li>{post.title}</li>"
-        return HttpResponse(f"<html><body><ul>{posts}</ul></body></html>")
+        posts = ''
+        for post in context['post_data']:
+            posts += f'<li>{post.title}</li>'
+        return HttpResponse(f'<html><body><ul>{posts}</ul></body></html>')
+
 
 class PostDetailView(DetailView):
     model = Post
@@ -37,3 +38,18 @@ class PostDetailView(DetailView):
                             f'<li>Body: {post.body}</li>'
                             f'<li>User: {post.user.first_name}</li>'
                             f'</ul></body></html>')
+
+
+class CommentListView(ListView):
+    model = Comment
+    context_object_name = 'comment_data'
+
+    def get_queryset(self):
+        self.queryset = Comment.objects.filter(post__id=self.kwargs['post_id'])
+        return self.queryset
+
+    def render_to_response(self, context, **response_kwargs):
+        comments = ''
+        for comment in context['comment_data']:
+            comments += f'<li>{comment.body}</li>'
+        return HttpResponse(f'<html><body><ul>{comments}</ul></body></html>')
